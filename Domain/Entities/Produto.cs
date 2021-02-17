73e2490a84acff9a39c433d41b7463b743255883
Rodrigo Abreu <app.abreu.solutions@outlook.com>
2021-02-17 15:42:24 -0300

    Add files via upload

---
+++ Domain/Entities/Produto.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string DataCadastro { get; set; }
+        public string Nome { get; set; }
+        public string CodigoDeBarras { get; set; }
+        public string Descricao { get; set; }
+        public string Preco { get; set; }
+        public string CriadoPor { get; set; }
+        public string UltimaModificacaoPor { get; set; }
+        public string UltimaModificacao { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ Domain/Entities/UsuarioLogin.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities
+{
+    public class UsuarioLogin
+    {
+        public int Id { get; set; }
+        public int IdUsuario { get; set; }
+        public string DataCadastro { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Status { get; set; }
+    }
+}
