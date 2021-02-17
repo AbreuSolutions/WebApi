6c3b0f128a029bf7f3d04dcbe54913980d1b406d
Rodrigo Abreu <app.abreu.solutions@outlook.com>
2021-02-17 12:08:12 -0300

    Add files via upload

---
+++ WebApi_Test/Models/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Application;
+using System;
+using System.Security.Claims;
+
+namespace WebApi_Test
+{
+    public class AppUser : ClaimsPrincipal
+    {
+        public AppUser(ClaimsPrincipal principal) : base(principal) { }
+        public int Key { get { return Convert.ToInt32(Util.Descrypt(this.FindFirst("Key").Value)); } }
+        public string Nome { get { return Util.Descrypt(this.FindFirst("Nome").Value); } }
+        public string Email { get { return Util.Descrypt(this.FindFirst("Email").Value); } }
+        public string Perfil { get { return Util.Descrypt(this.FindFirst("Perfil").Value); } }
+        public string Status { get { return Util.Descrypt(this.FindFirst("Status").Value); } }
+    }
+}
