30f1fed866a6807a18b5c9294aba8bf59dbe3d11
Rodrigo Abreu <app.abreu.solutions@outlook.com>
2021-02-17 12:06:58 -0300

    Add files via upload

---
+++ Infrastructure.Persistence/Migrations/20200724181511_Updat.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class Updat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TB_Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false).Annotation("SqlServer:Identity", "1, 1"),
+                    DataCadastro = table.Column<DateTime>(nullable: false),
+                    CriadoPor = table.Column<string>(nullable: true),
+                    UltimaModificacaoPor = table.Column<string>(nullable: true),
+                    UltimaModificacao = table.Column<DateTime>(nullable: true),
+                    Nome = table.Column<string>(nullable: true),
+                    CodigoDeBarras = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: true),
+                    Preco = table.Column<decimal>(type: "decimal(18,6)", nullable: false),
+                    Status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TB_Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false).Annotation("SqlServer:Identity", "1, 1"),
+                    DataCadastro = table.Column<DateTime>(nullable: false),
+                    DataNascimento = table.Column<DateTime>(nullable: false),
+                    Nome = table.Column<string>(nullable: true),
+                    SobreNome = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Telefone = table.Column<string>(nullable: true),
+                    Perfil = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TB_Usuario_Login",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false).Annotation("SqlServer:Identity", "1, 1"),
+                    IdUsuario = table.Column<int>(nullable: false),
+                    DataCadastro = table.Column<DateTime>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    Status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario_Login", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable("TB_Produtos");
+            migrationBuilder.DropTable("TB_Usuario");
+            migrationBuilder.DropTable("TB_Usuario_Login");
+        }
+    }
+}
