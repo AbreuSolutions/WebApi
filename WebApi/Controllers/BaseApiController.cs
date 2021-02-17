92e7623260093837ecd256e079f91d1ae448d3a4
Rodrigo Abreu <app.abreu.solutions@outlook.com>
2021-02-17 12:07:41 -0300

    Add files via upload

---
+++ WebApi/Controllers/BaseApiController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:ApiVersion}/[controller]")]
+    public abstract class BaseApiController : ControllerBase
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+    }
+}
---
+++ WebApi/Startup.cs
@@ -0,0 +1,86 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public IConfiguration _config { get; }
+        public Startup(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+                options.LoginPath = "/Início";
+                //options.LogoutPath = "/LogOut";
+            });
+
+            services.AddMemoryCache();
+            services.AddMvc();
+            services.AddControllersWithViews();
+
+            //services.AddApplicationLayer();
+            //services.AddIdentityInfrastructure(_config);
+            //services.AddPersistenceInfrastructure(_config);
+            //services.AddSharedInfrastructure(_config);
+            //services.AddSwaggerExtension();
+            //services.AddControllers();
+            //services.AddApiVersioningExtension();
+            //services.AddHealthChecks();
+            //services.AddScoped<IAuthenticatedUserService, AuthenticatedUserService>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            //app.UseHttpsRedirection();
+            //app.UseRouting();
+            //app.UseAuthentication();
+            //app.UseAuthorization();
+            //app.UseSwaggerExtension();
+            //app.UseErrorHandlingMiddleware();
+            //app.UseHealthChecks("/health");
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
+        }
+    }
+}
